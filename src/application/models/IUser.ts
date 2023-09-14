a5e9a2a466dece29fb2f35aa5920b264537e84c3
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-09-14 15:24:50 -0300

    updates

--- src/application/models/IUser.ts
+++ src/application/models/IUser.ts
@@ -1,1 +1,9 @@
-export interface IUser {}
+export interface IUser {
+  id?: string;
+  type: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
